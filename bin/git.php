85aba6d40d9e3af0f81eb6fbc68e64f37d9ac511
Guillaume Ponçon <guillaume.poncon@moarty.com>
2018-04-21 10:23:08 +0200

    Git subtree command

---
+++ bin/git.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+
+include dirname(__DIR__) . '/vendor/autoload.php';
+
+use Osf\Github\SubtreeManager;
+use Osf\Github\SubtreeConfig as Config;
+use Osf\Github\Sync\Components as OsfComponents;
+
+SubtreeManager::setConfig((new Config())
+    ->setGithubComponentsAddress('git@github.com:osflab')
+    ->setGithubContainerAddress('git@github.com:osflab/osf')
+    ->setComponentsPrefixDir('src')
+    ->setCurrentBranch('3.0')
+    ->setWorkDir(realpath(__DIR__ . '/..'))
+    ->setComponents(OsfComponents::COMPONENT_LIST));
+
+SubtreeManager::run();
